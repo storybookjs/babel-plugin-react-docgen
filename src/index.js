3cb3261013397ee42da325ba244890e532c8a94f
Madushan Nishantha <j.l.madushan@gmail.com>
2016-10-11 16:36:01 +0530

    __docgenInfo is now a object

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import {find} from 'lodash';
+import * as _ from 'lodash';
 import * as reactDocs from 'react-docgen';
 
 export default function({types: t}) {
   function isReactLikeClass(node) {
-    return !!find(node.body.body, classMember => {
+    return !!_.find(node.body.body, classMember => {
       return (
         t.isClassMethod(classMember) &&
           t.isIdentifier(classMember.key, { name: 'render' })
@@ -14,16 +14,59 @@ export default function({types: t}) {
   return {
     visitor: {
       Class(path) {
-          if(isReactLikeClass(path.node) && t.isProgram(path.parentPath.node)) {
-          const docgenInfo = t.expressionStatement(
-            t.assignmentExpression(
-              "=",
-              t.memberExpression(t.identifier(path.node.id.name), t.identifier('__docgenInfo')),
-              t.stringLiteral(JSON.stringify(reactDocs.parse(this.file.code)))
-            ));
-          path.parentPath.pushContainer("body", docgenInfo);
+        const {
+          node,
+          scope,
+        } = path;
+
+        if(isReactLikeClass(node)){
+          injectReactDocgenInfo(path, this.file.code, t);
         }
       }
     }
   };
+}
+
+
+function injectReactDocgenInfo(path, code, t) {
+  if(!t.isProgram(path.parentPath.node)) {
+    return;
+  }
+  const docObj = reactDocs.parse(code);
+  const docgenInfo = t.expressionStatement(
+    t.assignmentExpression(
+      "=",
+      t.memberExpression(t.identifier(path.node.id.name), t.identifier('__docgenInfo')),
+      buildObjectExpression(docObj, t)
+    ));
+  path.parentPath.pushContainer("body", docgenInfo);
+}
+
+function buildObjectExpression(obj, t){
+  if(_.isPlainObject(obj)) {
+    const children = [];
+    for (let key in obj) {
+      if(!obj.hasOwnProperty(key) || _.isUndefined(obj[key])) continue;
+      children.push(
+        t.objectProperty(
+          t.identifier(key),
+          buildObjectExpression(obj[key], t)
+        ));
+    }
+    return t.objectExpression(children);
+  } else if (_.isString(obj)) {
+    return t.stringLiteral(obj);
+  } else if (_.isBoolean(obj)) {
+    return t.booleanLiteral(obj);
+  } else if (_.isNumber(obj)){
+    return t.numericLiteral(obj);
+  } else if (_.isArray(obj)) {
+    const children = [];
+    obj.forEach(function(val) {
+      children.push(buildObjectExpression(val, t));
+    });
+    return t.ArrayExpression(children);
+  } else if(_.isNull(obj)) {
+    return t.nullLiteral();
+  }
 }
