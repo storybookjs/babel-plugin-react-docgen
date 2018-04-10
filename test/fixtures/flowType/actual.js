3ff310303508bfd8a9497f8fa0e772554722a67f
Daniel Duan <dduan@squarespace.com>
2018-04-10 16:44:14 -0400

    docgen includes docblock info about types

--- test/fixtures/flowType/actual.js
+++ test/fixtures/flowType/actual.js
@@ -9,6 +9,10 @@ type PropsType = {
 }
 
 class FlowTypeButton extends React.Component<PropsType> {
+  /**
+   * handle click number of times clicked and update parent component via callback
+   * @return  {string} returns nothing but at least this makes it into docgen
+   */
   handleClick = (bar?: string) => {
     console.log(bar);
   };
